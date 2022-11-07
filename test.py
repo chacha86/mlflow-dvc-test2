1a6f4657af93587a11c1e165acf01168328bf2fe
chacha86 <taejincha0619@gmail.com>
2022-11-07 19:46:15 +0900

    person data add

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import mlflow.sklearn
 
 data_url = dvc.api.get_url(
     path="data.txt",
-    repo='https://github.com/iterative/dataset-registry',
-    rev=""
+    repo='https://github.com/chacha86/mlflow-dvc-test2.git',
+    rev="68be4afcc135f1b4e108d5beef4f8f41357cf102"
 )
 mlflow.log_param("alpha", 1.0)
+
 
